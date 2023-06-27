eece17f943b675d1da15e445e11606b042979723
Vinícius Fernandes <viniciusfernandesdev@gmail.com>
2023-06-27 13:34:03 -0300

    feat(app): 🎉 desktop design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "purple": "hsl(259, 100%, 65%)",
+        "light-red": "hsl(0, 100%, 67%)",
+
+        "white": "hsl(0, 0%, 100%)",
+        "off-white": "hsl(0, 0%, 94%)",
+        "light-grey": "hsl(0, 0%, 86%)",
+        "smokey-grey": "hsl(0, 1%, 44%)",
+        "off-black": "hsl(0, 0%, 8%)",
       },
+      borderRadius: {
+        "4xl": "5rem",
+      },
+      fontSize: {
+        "2xs": "0.5rem",
+      }
     },
   },
   plugins: [],
-}
+};
+
